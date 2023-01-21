65ee64d26a0bad0ae24d2d5dce52e6ae6fcadc10
Parth Agrawal <aparth443@gmail.com>
2023-01-21 15:09:59 +0530

    solution-2

--- src/main/java/com/driver/Boat.java
+++ src/main/java/com/driver/Boat.java
@@ -1,8 +1,13 @@
 package com.driver;
 
 public class Boat implements WaterVehicle {
-    private String name;
-    private int capacity;
+    public String name;
+    public int capacity;
+
+    public Boat(String name,int capacity){
+        this.name = name;
+        this.capacity = capacity;
+    }
     public String getVehicleName(){
         return name;
     }
